de39992542abf47205ac68b99ba5f36f8c5197ff
Dingnh <vongolading@gmail.com>
2021-11-07 18:39:14 +0800

    Add HomePageSkeleton

--- src/Landing/skeletons/HomePageSkeleton.svelte
+++ src/Landing/skeletons/HomePageSkeleton.svelte
@@ -1,60 +1,24 @@
 <script>
-	import Skeleton from "svelte-skeleton";
+  import Skeleton from "svelte-skeleton";
 </script>
 
 <Skeleton
-	height="700"
-	width="100%"
-	primaryColor="#2c2838"
-	secondaryColor="#363242"
+  height="1000"
+  width="100%"
+  primaryColor="#E4E6E8"
+  secondaryColor="#F5F6F7"
 >
-	<rect
-		width="84"
-		height="20"
-		x="50%"
-		y="0"
-		rx="3"
-		ry="3"
-		transform="translate(-42 0)"
-	/>
-	<rect
-		width="700"
-		height="90"
-		x="50%"
-		y="35"
-		rx="3"
-		ry="3"
-		transform="translate(-350 0)"
-	/>
+  <rect width="100%" height="100" x="0%" y="35" rx="5" ry="5" />
 
-	<rect
-		width="200"
-		height="40"
-		x="50%"
-		y="145"
-		rx="3"
-		ry="3"
-		transform="translate(-100 0)"
-	/>
-
-	<rect width="50" height="50" x="0%" y="400" rx="3" ry="3" />
-	<rect
-		width="50"
-		height="50"
-		x="100%"
-		y="400"
-		rx="3"
-		ry="3"
-		transform="translate(-50 0)"
-	/>
-
-	<rect
-		width="800"
-		height="400"
-		x="50%"
-		y="250"
-		rx="3"
-		ry="3"
-		transform="translate(-400 0)"
-	/>
+  <rect width="49%" height="350" x="0%" y="200" rx="5" ry="5" />
+  <rect width="25%" height="75" x="52%" y="250" rx="5" ry="5" />
+  <rect width="49%" height="215" x="52%" y="335" rx="5" ry="5" />
+  <rect width="35%" height="75" x="0%" y="600" rx="5" ry="5" />
+  <rect width="180" height="180" x="0" y="700" rx="5" ry="5" />
+  <rect width="180" height="180" x="200" y="700" rx="5" ry="5" />
+  <rect width="180" height="180" x="400" y="700" rx="5" ry="5" />
+  <rect width="180" height="180" x="600" y="700" rx="5" ry="5" />
+  <rect width="180" height="180" x="800" y="700" rx="5" ry="5" />
+  <rect width="180" height="180" x="1000" y="700" rx="5" ry="5" />
+  <rect width="180" height="180" x="1200" y="700" rx="5" ry="5" />
 </Skeleton>
